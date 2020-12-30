680fe4f62529030ad8e64d86c196031524501633
aka-andi <andrea.motas@gmail.com>
2020-12-30 16:38:47 -0500

    add user input and chat with tag recognition

--- main.py
+++ main.py
@@ -79,3 +79,28 @@ try:
 except:
     model.fit(training, out, n_epoch=1000, batch_size=8, show_metric=True)
     model.save("model.tflearn")
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return np.array(bag)
+
+def chat():
+    print("this is your bot speaking")
+    while True:
+        inpt = input("Me: ")
+        if inpt.lower() == "quit":
+            break
+        
+        out = model.predict([bag_of_words(inpt, words)])
+        tag = labels[np.argmax(out)]
+
+chat()
